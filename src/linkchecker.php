b01e282b215779f8bfa35f3538e7d06fdd54f22f
marco <marco@webguerilla.net>
2019-03-22 17:46:25 +0100

    added form login support and fixed security issue

--- src/linkchecker.php
+++ src/linkchecker.php
@@ -34,7 +34,7 @@ function load_link_checker_admin_scripts($hook) {
 	if ($hook == 'toplevel_page_link-checker' || $hook == 'link-checker_page_link-checker-scheduler') {
 		wp_enqueue_script('jquery');
 
-		$linkcheckerURL = plugins_url('js/linkchecker-1.14.2.min.js', __FILE__);
+		$linkcheckerURL = plugins_url('js/linkchecker-1.15.0.min.js', __FILE__);
 		wp_enqueue_script('link_checker_linkcheckerjs', $linkcheckerURL);
 		wp_add_inline_script('link_checker_linkcheckerjs', "jQuery(document).ready(function() { riot.mount('*', { linkchecker: riot.observable() }); });");
 
@@ -101,6 +101,9 @@ function link_checker_page() {
 				else if ($isdev && $_GET['dev'] == '2') {
 					$websiteURLs = array('https://www.marcobeierer.com/', 'https://www.marcobeierer.ch/');
 				}
+				else if ($isdev && $_GET['dev'] == '3') {
+					$websiteURLs = array('https://www.aboutcms.de/');
+				}
 				else {
 					localhostCheck(); // only if not in dev mode
 				}
@@ -121,7 +124,7 @@ function link_checker_page() {
 				<?php $firstWebsite = true; ?>
 				<?php foreach ($websiteURLs as $websiteURL): ?>
 					<li role="presentation" class="<?php if ($firstWebsite) { echo 'active'; } ?>">
-						<a href="#<?php echo md5($websiteURL); ?>" aria-controls="<?php echo md5($websiteURL); ?>" role="tab" data-toggle="tab"><?php echo $websiteURL; ?></a>
+						<a href="#<?php echo md5($websiteURL); ?>" aria-controls="<?php echo md5($websiteURL); ?>" role="tab" data-toggle="tab"><?php echo esc_attr($websiteURL); ?></a>
 					</li>
 					<?php $firstWebsite = false; ?>
 				<?php endforeach; ?>
@@ -138,12 +141,16 @@ function link_checker_page() {
 						<div role="tabpanel" class="tab-pane <?php if ($firstWebsite) { echo 'active'; } ?>" id="<?php echo md5($websiteURL); ?>">
 							<linkchecker
 								id="<?php echo $count; ?>"
-								website-url="<?php echo $websiteURL; ?>"
-								token="<?php echo get_option('link-checker-token'); ?>"
+								website-url="<?php echo esc_attr($websiteURL); ?>"
+								token="<?php echo esc_attr(get_option('link-checker-token')); ?>"
 								origin-system="wordpress"
 								max-fetchers="<?php echo (int) get_option('link-checker-max-fetchers', 3); ?>"
 								enable-scheduler="true"
-								email="<?php echo get_option('admin_email'); ?>"
+								email="<?php echo esc_attr(get_option('admin_email')); ?>"
+								edit-urls-endpoint="admin-ajax.php?action=link_checker_edit_urls"
+								login-page-url="<?php echo esc_attr(get_option('link-checker-login-page-url')); ?>"
+								login-form-selector="<?php echo esc_attr(get_option('link-checker-login-form-selector')); ?>"
+								login-data="<?php echo esc_attr(get_option('link-checker-login-data')); ?>"
 							>
 							</linkchecker>
 						</div>
@@ -155,13 +162,16 @@ function link_checker_page() {
 				</div>
 			<?php else: ?>
 				<linkchecker
-					website-url="<?php echo $websiteURLs[0]; ?>"
-					token="<?php echo get_option('link-checker-token'); ?>"
+					website-url="<?php echo esc_attr($websiteURLs[0]); ?>"
+					token="<?php echo esc_attr(get_option('link-checker-token')); ?>"
 					origin-system="wordpress"
 					max-fetchers="<?php echo (int) get_option('link-checker-max-fetchers', 3); ?>"
 					enable-scheduler="true"
-					email="<?php echo get_option('admin_email'); ?>"
+					email="<?php echo esc_attr(get_option('admin_email')); ?>"
 					edit-urls-endpoint="admin-ajax.php?action=link_checker_edit_urls"
+					login-page-url="<?php echo esc_attr(get_option('link-checker-login-page-url')); ?>"
+					login-form-selector="<?php echo esc_attr(get_option('link-checker-login-form-selector')); ?>"
+					login-data="<?php echo esc_attr(get_option('link-checker-login-data')); ?>"
 				>
 				</linkchecker>
 			<?php endif; ?>
@@ -226,26 +236,6 @@ function link_checker_scheduler_page() {
 		<div class="bootstrap3">
 			<h2>Link Checker Scheduler</h2>
 			<p>The scheduler was moved in version 1.12.0 to a tab in the main view of Link Checker.</p>
-
-<!--
-			<?php
-				$url = get_home_url();
-				$dev = isset($_GET['dev']);
-
-				if ($dev) {
-					$url = 'https://www.marcobeierer.com/';
-				} 
-				else {
-					localhostCheck(); // only if not in dev mode
-				}
-			?>
-
-			<linkchecker-scheduler
-				website-url="<?php echo $url; ?>"
-				token="<?php echo get_option('link-checker-token'); ?>"
-				email="<?php echo get_option('admin_email'); ?>">
-			</linkchecker-scheduler>
--->
 		</div>
 	</div>
 <?php
@@ -259,33 +249,64 @@ function register_link_checker_settings_page() {
 function register_link_checker_settings() {
 	register_setting('link-checker-settings-group', 'link-checker-token');
 	register_setting('link-checker-settings-group', 'link-checker-max-fetchers', 'intval');
+	register_setting('link-checker-settings-group', 'link-checker-login-page-url');
+	register_setting('link-checker-settings-group', 'link-checker-login-form-selector');
+	register_setting('link-checker-settings-group', 'link-checker-login-data');
 }
 
 function link_checker_settings_page() {
 ?>
 	<div class="wrap">
-		<h2>Link Checker Settings</h2>
-		<div class="card">
-			<form method="post" action="options.php">
-				<?php settings_fields('link-checker-settings-group'); ?>
-				<?php do_settings_sections('link-checker-settings-group'); ?>
-				<h3>Your Token</h3>
-				<p><textarea name="link-checker-token" style="width: 100%; min-height: 350px;"><?php echo esc_attr(get_option('link-checker-token')); ?></textarea></p>
-				<p>The Link Checker allows you to check up to 500 internal and external links for free. If your website has more links, you can buy a token for the <a href="https://www.marcobeierer.com/wordpress-plugins/link-checker-professional">Link Checker Professional</a> to check up to 50'000 links.</p>
-				<p>The professional version also checks if you have broken embedded images on your site.</p>
+		<h1>Link Checker Settings</h1>
+		<form method="post" action="options.php">
+			<?php settings_fields('link-checker-settings-group'); ?>
+			<?php do_settings_sections('link-checker-settings-group'); ?>
+			<div style="display: flex; flex-wrap: wrap; margin-left: -5px; margin-right: -5px;">
+				<div class="card" style="margin: 5px;">
+					<h2>General Settings</h2>
+					<h3>Your Token</h3>
+					<p><textarea name="link-checker-token" style="width: 100%; min-height: 350px;"><?php echo esc_attr(get_option('link-checker-token')); ?></textarea></p>
+					<p>The Link Checker allows you to check up to 500 internal and external links for free. If your website has more links, you can buy a token for the <a href="https://www.marcobeierer.com/wordpress-plugins/link-checker-professional">Link Checker Professional</a> to check up to 50'000 links.</p>
+					<p>The professional version also checks if you have broken embedded images on your site.</p>
 
-				<h3>Concurrent Connections</h3>
-				<p>
-					<select name="link-checker-max-fetchers" style="width: 100%;">
-					<?php for ($i = 1; $i <= 10; $i++) { ?>
-						<option <?php if ((int) get_option('link-checker-max-fetchers', 3) === $i) { ?>selected<?php } ?> value="<?php echo $i; ?>"><?php echo $i; ?></option>
-					<?php } ?>
-					</select>
-				</p>
-				<p>Number of the maximal concurrent connections. The default value is three concurrent connections, but some hosters do not allow three concurrent connections or an installed plugin may use that much resources on each request that the limitations of your hosting is reached with three concurrent connections. With this option you can limit the number of concurrent connections used to access your website and make the Link Checker work under these circumstances. You can also increase the number of concurrent connections if your server can handle it.</p>
-				<?php submit_button(); ?>
-			</form>
-		</div>
+					<h3>Concurrent Connections</h3>
+					<p>
+						<select name="link-checker-max-fetchers" style="width: 100%;">
+						<?php for ($i = 1; $i <= 10; $i++) { ?>
+							<option <?php if ((int) get_option('link-checker-max-fetchers', 3) === $i) { ?>selected<?php } ?> value="<?php echo $i; ?>"><?php echo $i; ?></option>
+						<?php } ?>
+						</select>
+					</p>
+					<p>Number of the maximal concurrent connections. The default value is three concurrent connections, but some hosters do not allow three concurrent connections or an installed plugin may use that much resources on each request that the limitations of your hosting is reached with three concurrent connections. With this option you can limit the number of concurrent connections used to access your website and make the Link Checker work under these circumstances. You can also increase the number of concurrent connections if your server can handle it.</p>
+					<?php submit_button(); ?>
+				</div>
+				<div class="card" style="margin: 5px;">
+					<h2>Form Login Settings (Professional only)</h2>
+					<p>The form login feature allows you to setup a form login that the Link Checker uses to login to your site. This for example allows the Link Checker to check a membership area.</p>
+					<p>Please be very <strong>CAREFUL</strong> with this feature and always backup your site before using it because using this feature can lead to <strong>DATA LOSS</strong> if you make a mistake!</p>
+					<p>It is highly recommended that you create and use a dedicated read-only account for the Link Checker. Read-only because the Link Checker simulates a click on every link on your website and if you for example have an <em>delete button</em>, the Link Checker clicks on it and may delete data if write access is given. A dedicated account is highly recommended because it is technically necessary to store the <strong>PASSWORD IN PLAINTEXT</strong>. You should thus not use this password anywhere else. If you use the scheduler, the provided password is also saved on the Link Checker server.</p>
+					<p>If your login area contains very <strong>sensitive information</strong> that should be protected under all circumstances, you should <strong>not use this feature</strong> at all.</p>
+					<p>Please read the instructions on my website carefully before using this feature:<br />
+					<a target="_blank" href="https://www.marcobeierer.com/tools/link-checker/form-login-instructions">Form Login Instructions</a></p>
+
+					<h3>Login Page URL</h3>
+					<p><input type="text" name="link-checker-login-page-url" style="width: 100%" value="<?php echo esc_attr(get_option('link-checker-login-page-url')); ?>" /></p>
+					<p>The URL of the login page. The URL can be absolute or relative to the website URL.</p>
+
+					<h3>Form Selector</h3>
+					<p><input type="text" name="link-checker-login-form-selector" style="width: 100%" value="<?php echo esc_attr(get_option('link-checker-login-form-selector')); ?>" /></p>
+					<p>The DOM query selector to select the login form on the login page, for example <em>#loginform</em> or <em>.loginform</em>.</p>
+					<p>See the instructions on my website for details. There is also an explanation about how to find the selector.</p>
+
+					<h3>Data (Username and Password)</h3>
+					<p><input type="text" name="link-checker-login-data" style="width: 100%" value="<?php echo esc_attr(get_option('link-checker-login-data')); ?>" /></p>
+					<p>Provide all data necessary to login in the POST query format, for example <em>username=xyz&password=qwerty</em>. If you have special characters in your username or password, the values have to be percent encoded.</p>
+					<p>See the instructions on my website for details about how to find this information.</p>
+
+					<?php submit_button(); ?>
+				</div>
+			</div>
+		</form>
 	</div>
 <?php
 }
